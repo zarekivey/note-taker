8b78fe46829ce17c1b73b774117723ff925c8ad7
Malachi Ivey <zarekbusiness@gmail.com>
2019-01-25 08:35:01 -0500

    adding add note functionality

--- notes.js
+++ notes.js
@@ -1,7 +1,28 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 const addNote = (title, body) => {
-    console.log('Adding note', title, body)
+    let notes = [];
+    const note = {
+       title,
+       body 
+    };
+
+    try {
+        // fetching the notes
+        const notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);        
+    } catch (e) {
+
+    }
+
+    let duplicateNotes = notes.filter((note) => note.title === title);
+    // adding the new note to the array of notes, if its not a dupe
+    if (duplicateNotes.length === 0) {
+        notes.push(note); 
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+    }
 };
 
 const getAll = () => {
